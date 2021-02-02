abc24723f262bdf9653c105dd4598071dd10d462
tohidur <tohidur@urbanpiper.com>
2021-02-02 17:30:08 +0530

    terraform aws basic - start with aws vpc network setup

---
+++ aws/variables.tf
@@ -0,0 +1,30 @@
+# core
+variable "region" {
+  description = "The AWS region to create resources in."
+  default     = "ap-south-1"
+}
+
+
+# networking
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
